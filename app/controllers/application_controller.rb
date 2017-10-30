a7dbb5dbd67865536fa27335b81285488fc20e80
Brandon Stewart <boveus@gmail.com>
2017-10-30 15:47:44 -0600

    add initial user dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-   @current_user ||= User.find(session[:user_id])
+   @current_user ||= User.find_by(uid: session[:uid])
   end
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,5 @@
 class UserController < ApplicationController
   def show
+    flash[:message] = "Welcome, #{current_user.username}"
   end
 end
