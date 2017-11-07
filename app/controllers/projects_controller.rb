eae44f4c63e1c3f73b3e9ba29ba9eca3ab009b22
Brandon Stewart <boveus@gmail.com>
2017-11-07 16:04:18 -0700

    add image uploading functionality

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,20 +3,27 @@ class ProjectsController < ApplicationController
     @accepted_projects = Project.accepted
     @projects_under_review = Project.under_review
     @proposed_projects = Project.proposed
-  end
-
-  def new
     @project = Project.new
   end
 
+  def new
+  end
+
   def create
-    Project.create(project_params)
+    binding.pry
+    image_path = Project.handle_image_upload(image_param["image_path"])
+    project = Project.create(project_params)
+    project.update(image_path: image_path)
     redirect_to projects_path
   end
 
   private
 
   def project_params
-    params.require("project").permit("title", "description", "image_path", "github_link", "heroku_link")
+    params.require("project").permit("title", "description", "github_link", "heroku_link")
+  end
+
+  def image_param
+    params.require("project").permit("image_path")
   end
 end
