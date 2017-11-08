f4c42d297c7c4aab91530eb70b1eff06a881fbac
Brandon Stewart <boveus@gmail.com>
2017-11-07 20:39:54 -0700

    maybe a fix

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { :size => 1 }
   config.redis = { url: ENV['REDISTOGO_URL'] }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { :size => 1 }
   config.redis = { url: ENV['REDISTOGO_URL'] }
 end
