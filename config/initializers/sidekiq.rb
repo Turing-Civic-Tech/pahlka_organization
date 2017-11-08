476e6323f2e51b2ea4e555b466c95ef9ce8d6973
Brandon Stewart <boveus@gmail.com>
2017-11-07 20:11:03 -0700

    add redis size 1

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,9 @@
 Sidekiq.configure_server do |config|
+  config.redis = { :size => 1 }
   config.redis = { url: ENV['REDISTOGO_URL'] }
 end
 
 Sidekiq.configure_client do |config|
+  config.redis = { :size => 1 }
   config.redis = { url: ENV['REDISTOGO_URL'] }
 end
